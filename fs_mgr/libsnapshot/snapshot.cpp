20933df73682ade330310a0953616d78f49096ad
basamaryan <basam.aryan@gmail.com>
2025-07-09 00:16:39 -0400

    fs_mgr: snapshot: Downgrade log level for state file read failures to WARNING
    Change-Id: I27c2b46f5891fde6afd3d18a5dc1d975c8eca1a6

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -3203,7 +3203,7 @@ SnapshotUpdateStatus SnapshotManager::ReadSnapshotUpdateStatus(LockedFile* lock)
     SnapshotUpdateStatus status = {};
     std::string contents;
     if (!android::base::ReadFileToString(GetStateFilePath(), &contents)) {
-        PLOG(ERROR) << "Read state file failed";
+        PLOG(WARNING) << "Read state file failed";
         status.set_state(UpdateState::None);
         return status;
     }
